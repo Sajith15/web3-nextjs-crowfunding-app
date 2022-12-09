321cdcecd78d3af607d025b2475c526945faaed9
Sajith15 <kpsajith2000@gmail.com>
2022-12-10 00:44:41 +0530

    add all campaigns components, chain details

---
+++ src/common/hooks/chainDetails/testnetDetails.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const CHAIN_IDS = [80001];
+
+export const POLYGON_TESTNET_CHAIN_ID = 80001;
+
+export const testnetDetails = () => {
+  return <div>testnetDetails</div>;
+};
